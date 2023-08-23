b59f09dc392ceca1487b89435f99325cfbca3086
NevilKumar2002 <127580266+NevilKumar2002@users.noreply.github.com>
2023-08-23 12:15:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+        function displayBrowserInfo() {
+            // Get browser name and version from navigator object
+            const browserName = navigator.appName;
+            const version = navigator.appVersion;
+
+            // Create the message
+            const message = "You are using " + browserName + " version " + version;
+
+            // Display the message in the browser-info div
+            const browserInfoDiv = document.getElementById("browser-info");
+            browserInfoDiv.textContent = message;
+		}
+window.onload = displayBrowserInfo;
